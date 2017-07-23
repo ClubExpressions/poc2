9c53c5e7fe67e9e71662857cb8e7ef790a719aa7
Christophe Gragnic <cgragnic@protonmail.com>
2017-07-23 09:33:38 +0200

    Install the kinto npm module

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ window.deps = {
     'react-dom' : require('react-dom'),
     'react-mathjax' : require('react-mathjax'),
     'clubexpr' : require('clubexpr'),
+    'kinto' : require('kinto'),
 };
 
 window.React = window.deps['react'];
