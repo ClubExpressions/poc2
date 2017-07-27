334b24b04a4431ecbf89e2a12c81e2d4074ff7e7
Christophe Gragnic <cgragnic@protonmail.com>
2017-07-27 10:58:55 +0200

    Install auth0-js

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ window.deps = {
     'react-mathjax' : require('react-mathjax'),
     'clubexpr' : require('clubexpr'),
     'kinto' : require('kinto'),
+    'auth0' : require('auth0-js'),
 };
 
 window.React = window.deps['react'];
