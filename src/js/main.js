d8ab681e0950f98bccd5dd7996d447b90d36f4e9
Christophe Gragnic <cgragnic@protonmail.com>
2017-08-12 16:23:51 +0200

    Install react-mathjax-local

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ window.deps = {
     'react' : require('react'),
     'react-dom' : require('react-dom'),
     'react-bootstrap' : require('react-bootstrap'),
-    'react-mathjax' : require('react-mathjax'),
+    'react-mathjax' : require('react-mathjax-local'),
     'clubexpr' : require('clubexpr'),
     'kinto' : require('kinto'),
     'auth0' : require('auth0-js'),
